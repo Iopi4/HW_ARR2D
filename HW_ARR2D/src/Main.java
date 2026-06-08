18b0c186c9cbbf847c171b89378ba95db4124e16
Iopi4 <relentler@gmail.com>
2026-06-08 13:56:38 +0500

    Task_2 (Additionally)

--- HW_ARR2D/src/Main.java
+++ HW_ARR2D/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
@@ -17,14 +18,28 @@ public class Main {
             }
         }
         //Выведем на экран рандомный массив
+        System.out.println("Рандомный массив");
         printColors(colors);
         System.out.println();
 
-        //Вызовем функцию поворота рандомного массива на 90 градусов по часовой стрелке
-        rotateColors(colors, rotateColors);
+        Scanner scanner = new Scanner(System.in);
 
-        //Выведем на экран рандомный массив повернутый на 90 градусов по часовой стрелке
-        printColors(rotateColors);
+        while (true) {
+            System.out.println("Введите угол поворота рандомного массива на: 90, 180 или 270 градусов");
+
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
+            }
+            int corner = Integer.parseInt(input);
+
+            //Вызовем функцию поворота рандомного массива на выбранный угол 90, 180, 270 градусов по часовой стрелке
+            rotateColors(colors, rotateColors, corner);
+
+            //Выведем на экран рандомный массив повернутый на выбранный угол по часовой стрелке
+            printColors(rotateColors);
+            System.out.println();
+        }
     }
 
     //Оформим вывод матрицы на экран в виде отдельного метода
@@ -40,11 +55,27 @@ public class Main {
         }
     }
 
-    //Оформим поворот матрицы на 90 градусов по часовой стрелки в виде отдельного метода
-    public static void rotateColors(int[][] color, int[][] rotateColor) {
-        for (int i = 0; i < color.length; i++) {
-            for (int j = 0; j < color.length; j++) {
-                rotateColor[i][j] = color[SIZE - 1 - j][i];
+    //Оформим поворот массива на 90, 180 и 270 градусов по часовой стрелки в виде отдельного метода
+    public static void rotateColors(int[][] color, int[][] rotateColor, int corner) {
+        if (corner == 90) {
+            for (int i = 0; i < color.length; i++) {
+                for (int j = 0; j < color.length; j++) {
+                    rotateColor[i][j] = color[SIZE - 1 - j][i];
+                }
+            }
+        }
+        if (corner == 180) {
+            for (int i = 0; i < color.length; i++) {
+                for (int j = 0; j < color.length; j++) {
+                    rotateColor[i][j] = color[SIZE - 1 - i][SIZE - 1 - j];
+                }
+            }
+        }
+        if (corner == 270) {
+            for (int i = 0; i < color.length; i++) {
+                for (int j = 0; j < color.length; j++) {
+                    rotateColor[i][j] = color[j][SIZE - 1 - i];
+                }
             }
         }
     }
